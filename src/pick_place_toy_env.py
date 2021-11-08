0cf35c5f0c7de36e0281f636a4d3ec27f80a674a
Daniel Lee <DocDog@Yoon-Has-Mac-mini.local>
2021-11-08 16:29:10 -0500

    pick and place toy environment with TODOs

---
+++ src/pick_place_toy_env.py
@@ -0,0 +1,107 @@
+import logging
+import numpy as np
+import attr
+
+from robogym.envs.rearrange.common.base import (
+    RearrangeEnv,
+    RearrangeEnvConstants,
+    RearrangeEnvParameters,
+)
+
+from time import sleep
+
+from robogym.envs.rearrange.goals.object_stack_goal import ObjectStackGoal
+from robogym.envs.rearrange.simulation.base import RearrangeSimParameters
+from robogym.envs.rearrange.simulation.blocks import (
+    BlockRearrangeSim,
+    BlockRearrangeSimParameters,
+)
+
+from robogym.robot.composite.controllers.ur_gripper_arm import (
+    Direction,
+    URGripperArmController,
+)
+from robogym.robot_env import build_nested_attr
+
+logger = logging.getLogger(__name__)
+
+
+@attr.s(auto_attribs=True)
+class BlockStackEnvConstants(RearrangeEnvConstants):
+    # whether block stacked in the fixed order or random order.
+    fixed_order: bool = False
+
+
+@attr.s(auto_attribs=True)
+class BlockStackEnvParameters(RearrangeEnvParameters):
+    simulation_params: RearrangeSimParameters = build_nested_attr(
+        BlockRearrangeSimParameters, default=dict(num_objects=1)
+    )
+
+
+class BlockStackEnv(
+    RearrangeEnv[BlockStackEnvParameters, BlockStackEnvConstants, BlockRearrangeSim]
+):
+    @classmethod
+    def build_goal_generation(cls, constants, mujoco_simulation):
+        return ObjectStackGoal(
+            mujoco_simulation, constants.goal_args, constants.fixed_order
+        )
+
+# Our implementation starts here
+
+make_env = BlockStackEnv.build
+env = make_env()
+controller = URGripperArmController(env.unwrapped)
+robot = env.robot # environment's robot with 6DOF
+#print(env.unwrapped.mujoco_simulation.)
+obs = env.reset()
+#print(env.action_space)
+
+"""
+All potential information we can get from thh observation
+
+['obj_pos', 'obj_rel_pos', 'obj_vel_pos', 'obj_rot', 'obj_vel_rot', 'robot_joint_pos', \
+'gripper_pos', 'gripper_velp', 'gripper_controls', 'gripper_qpos', 'gripper_vel', 'qpos', \
+'qpos_goal', 'goal_obj_pos', 'goal_obj_rot', 'is_goal_achieved', 'rel_goal_obj_pos', \
+'rel_goal_obj_rot', 'obj_gripper_contact', 'obj_bbox_size', 'obj_colors', 'safety_stop', \
+'tcp_force', 'tcp_torque', 'action_ema']
+"""
+
+env.reset()
+for _ in range(1):
+    env.render()
+
+    # Image Extraction
+    a1 = env.render(mode="rgb_array", width=500,height=500) # Default renderer from robogym/robot_env.py class
+    a2 = env.sim.render(camera_name='phys_checks_cam', width=500, height=500) # Mujoco specific renderer
+    # Cameras available include: 'vision_cam_wrist', 'vision_cam_top', 'vision_cam_front', 'phys_checks_cam'
+    # TODO: How can we remove the goal shadow from the image? How can we add new types of cameras/change it?
+    
+    x = env.observe()
+    print(x.keys())
+    action = env.action_space.sample()
+    # TODO: What is the meaning of this action space and why is it a discrete 11?
+    print(env.action_space)
+    observation, reward, done, info = env.step(action)
+
+    obj_pos = observation['obj_pos']
+    obj_rot = observation['obj_rot']
+    goal_pos = observation['goal_obj_pos']
+    goal_rot = observation['goal_obj_rot']
+    l2_reward = 0
+    # TODO: Encode the reward function as an environment function and return from the iteration.
+    for i in range(len(obj_pos)):
+        l2_reward -= np.linalg.norm(np.array(obj_pos[i])-np.array(goal_pos[i]))
+    print(l2_reward)
+
+env.close()
+
+import matplotlib.pyplot as plt
+
+# Visualization of mujoco camera image
+print(np.shape(a2))
+plt.imshow(a2)
+plt.show()
+print("done!")
+
